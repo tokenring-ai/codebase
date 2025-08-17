7d34f53248366516767b557e6ad7a253480a8c5b
Mark Dierolf <mdierolf@cloudquote.io>
2025-08-17 07:54:10 -0700

    Continuing work on typescript migration

--- FileTreeResource.ts
+++ FileTreeResource.ts
@@ -1,44 +1,9 @@
 import {FileMatchResource} from "@token-ring/filesystem";
 
-export interface FileTreeItem {
-  path: string;
-  ignore?: string;
-}
-
-export interface FileTreeParams {
-  items: FileTreeItem[];
-}
-
 /**
  * Class representing a file tree context extending DirectoryService.
  */
 export default class FileTreeResource extends FileMatchResource {
-  static constructorProperties = {
-    items: {
-      type: "array",
-      description: "Files to match",
-      items: {
-        type: "object",
-        properties: {
-          path: {
-            type: "string",
-            required: true,
-            description: "Path to directory to include",
-          },
-          ignore: {
-            type: "string",
-            description:
-              "A .gitignore/node-glob ignore style list of files to ignore",
-          },
-        },
-      },
-    },
-  } as const;
   name = "FileTreeService";
   description = "Provides FileTree functionality";
-
-  constructor(params: FileTreeParams) {
-    // pass-through to base class which handles items
-    super(params as any);
-  }
 }
--- WholeFileResource.ts
+++ WholeFileResource.ts
@@ -1,22 +1,9 @@
 import {FileMatchResource} from "@token-ring/filesystem";
 
-export interface WholeFileItem {
-  path: string;
-  include: RegExp;
-}
-
-export interface WholeFileParams {
-  items: WholeFileItem[];
-}
-
 /**
  * Class representing a resource that yields whole files.
  */
 export default class WholeFileResource extends FileMatchResource {
   name = "WholeFileResource";
   description = "Provides whole files to include in the chat context";
-
-  constructor(params: WholeFileParams) {
-    super(params);
-  }
 }
